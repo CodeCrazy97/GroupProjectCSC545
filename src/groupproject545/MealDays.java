055acae17b151936273891ed4d6fab6b8dc20edd
Chris Ward <acward96@gmail.com>
2019-04-09 14:57:38 -0400

    Added get meals days by title

--- src/groupproject545/MealDays.java
+++ src/groupproject545/MealDays.java
@@ -1,5 +1,10 @@
 package groupproject545;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import oracle.jdbc.*;
+
 /*
 Class to hold all the meals served on certain days (Sunday, Monday, etc.) during
 certain meals (breakfast, brunch, lunch, etc.) for a specified meal plan.
@@ -28,6 +33,13 @@ public class MealDays {
         this.mealTitle = mealTitle;
         this.dayOfWeek = dayOfWeek;
     }
+    
+    public MealDays(String mealTitle, String mealName, String dayOfWeek, String mealPlanTitle) {
+        this.mealName = mealName;
+        this.mealTitle = mealTitle;
+        this.dayOfWeek = dayOfWeek;
+        this.mealPlanTitle = mealPlanTitle;
+    }
 
     public String getMealName() {
         return mealName;
@@ -44,5 +56,54 @@ public class MealDays {
     public String getMealPlanTitle() {
         return mealPlanTitle;
     }
+    
+    public void add() throws Exception{
+        Connection conn = ConnectDb.setupConnection();
+        OraclePreparedStatement addMealDay = null;
+        try {
+            String sqlStatement = "Insert into MEALDAY (dayOfWeek, mealTitle, mealName, mealPlanTitle) " 
+                    + "values (?, ?, ?, ?)";
+            addMealDay = (OraclePreparedStatement) conn.prepareStatement(sqlStatement);
+            addMealDay.setString(1, this.dayOfWeek);
+            addMealDay.setString(2, this.mealTitle);
+            addMealDay.setString(3, this.mealName);
+            addMealDay.setString(4, this.mealPlanTitle);
+            addMealDay.executeUpdate();
+            conn.commit();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+    /**
+     * Gets the meal plan based on the primary key
+     * @throws Exception 
+     */
+    public static List<MealDays> getByMealPlanTitle(String title) throws Exception {
+        List<MealDays> mealDaysList = new ArrayList<MealDays>();
 
+        Connection conn = ConnectDb.setupConnection();
+        String sqlStatement = "select * from MealDays where mealPlanTitle=?";
+        OraclePreparedStatement pst = (OraclePreparedStatement) conn.prepareStatement(sqlStatement);
+        pst.setString(1, title);
+        OracleResultSet rs = (OracleResultSet) pst.executeQuery();
+        try {
+            
+            while (rs.next()) {
+                String dayOfWeek = rs.getString(1);
+                String mealTitle = rs.getString(2);
+                String mealName = rs.getString(3);
+                String mealPlanTitle = rs.getString(4);
+                mealDaysList.add(new MealDays(mealTitle, mealName, dayOfWeek, mealPlanTitle));
+            }
+            System.out.println("5");
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } finally {
+            ConnectDb.close(rs);
+            ConnectDb.close(pst);
+            ConnectDb.close(conn);
+        }
+        return mealDaysList;
+    }
 }

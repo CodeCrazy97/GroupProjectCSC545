49e5dee2075965fd04e6c1595a182218ef7a5e38
EvanTWright <43044102+EvanTWright@users.noreply.github.com>
2019-04-16 14:58:33 -0400

    Added inserts to ingredients

--- createDatabase.sql
+++ createDatabase.sql
@@ -62,8 +62,37 @@ constraint fk_mealDay_mealname foreign key (mealName) references meals(name) on 
 constraint fk_mealDay_mealplantitle foreign key (mealPlanTitle) references mealPlan(title) on delete cascade
 );
 
-
-
-
-
-
+                        
+insert into ingredients values('broccoli','vegetables','Y','high in fiber, very high in vitamin C and has potassium, B6 and vitamin A'); 
+insert into ingredients values('tuna','fish','N','calories per 8oz: 116 calories');
+insert into ingredients values('hamburger','meat','N','Calories per single patty: 266');  
+insert into ingredients values('red delicious apple','fruit','Y','Calories per apple: 68');
+insert into ingredients values('turkey burger','meat','N','Caloriesper patty: 209');                           
+insert into ingredients values('asparagus','vegetables','N','Calories per stick: 7');
+insert into ingredients values('2% milk','dairy','Y','Calories per gallon: 1024');
+insert into ingredients values('almond milk','dairy','N','Calories per half gallon: 178');
+insert into ingredients values('heavy whipping cream','dairy','Y','Calories per ounce: 40');
+insert into ingredients values('angel hair','pasta','Y','Calories per 8 oz: 369');                           
+insert into ingredients values('wheat bread','grain','Y','Calories per loaf: 700');
+insert into ingredients values('salt','seasoning','Y','Calories per tablespoon: 15');
+insert into ingredients values('pepper','seasoning','N','Calories per tablespoon: 9');
+insert into ingredients values('texas pete hot sauce','seasoning','N','Calories per table spoon: 17');
+insert into ingredients values('alfredo sauce','seasoning','Y','Calories per 8oz: 296');                           
+insert into ingredients values('spaghetti noodles','pasta','N','Calories per 8oz: 166');
+insert into ingredients values('mahi mahi','fish','Y','Calories per 8oz: 320');
+insert into ingredients values('sour cream','dairy','N','Calories per 8oz: 154');
+insert into ingredients values('salmon','fish','N','Calories per 8oz: 460');
+insert into ingredients values('pineapple','fruit','Y','Calories per whole pineapple: 1266');                           
+insert into ingredients values('carrots','vegetables','Y','Calories per 8oz: 38');
+insert into ingredients values('green beans','vegetables','Y','Calories: 266');
+insert into ingredients values('orange','fruit','N','Calories per fruit: 97');
+insert into ingredients values('porkchop','meat','Y','Calories per chop: 250');
+insert into ingredients values('venision','meat','Y','Calories per 8oz: 300');                           
+insert into ingredients values('pear','fruit','Y','Calories per pear: 50');
+insert into ingredients values('watermelon','fruit','Y','Calories per watermelon: 686');
+insert into ingredients values('cocoa powder','seasoning','N','Calories per 8 oz: 196'); 
+insert into ingredients values('colby cheese','dairy','N','Calories per 8oz: 122');
+insert into ingredients values('pepperjack cheese','dairy','Y','Calories per 8oz: 85');                           
+insert into ingredients values('lemon pepper seasoing','seasoning','Y','Calories per 8oz: 18');
+                           
+                           

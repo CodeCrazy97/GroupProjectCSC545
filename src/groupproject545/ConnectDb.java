c6a65e7762430a5d6b44d3b37407eadfff773151
Ethan <39338996+CodeCrazy97@users.noreply.github.com>
2019-05-06 10:29:02 -0400

    Changed username, password, host connections for security reasons.

--- src/groupproject545/ConnectDb.java
+++ src/groupproject545/ConnectDb.java
@@ -19,10 +19,10 @@ public class ConnectDb {
         String jdbcDriver = "oracle.jdbc.driver.OracleDriver";
         
         // *** EKU connection ***
-        String jdbcUrl = "jdbc:oracle:thin:@cswinserv.eku.edu:1521:cscdb";  // URL for the database including the protocol (jdbc), the vendor (oracle), the driver (thin), the server (csshrpt.eku.edu), the port number (1521), database instance name (cscdb)
+        String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:db";  // URL for the database including the protocol (jdbc), the vendor (oracle), the driver (thin), the server (csshrpt.eku.edu), the port number (1521), database instance name (cscdb)
 
-        String username = "vaughan5452019";
-        String password = "2108";
+        String username = "username";
+        String password = "password";
 
         
         // Try to setup the connection.
